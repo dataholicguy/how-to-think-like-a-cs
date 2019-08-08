d809e1670e966028657205318727785266815a7c
dataholicguy <dataismgeek@gmail.com>
2019-08-08 15:19:34 +0700

    Solutions for Exercises in Chap 01

---
+++ chap01/ex01.py
@@ -0,0 +1,6 @@
+# Write an English sentence with understandable semantics but incorrect syntax.
+# Write another English sentence which has correct syntax but has semantic errors.
+
+s1 = 'I love my dog but he bite me usually'
+
+s2 = 'I want to understand Machine Learning without literature'
